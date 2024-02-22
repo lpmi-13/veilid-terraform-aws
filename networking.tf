0e0ec980c3b374a7bec43ed1715e2da3786ece07
Adam Leskis <leskis@gmail.com>
2024-02-22 15:06:15 +0000

    remove unneeded config

--- networking.tf
+++ networking.tf
@@ -7,8 +7,6 @@ resource "aws_subnet" "veilid-subnet" {
   vpc_id     = aws_vpc.veilid-vpc.id
   cidr_block = "10.0.1.0/24"
 
-  map_public_ip_on_launch = true
-
   tags = {
     Name = "Veilid-Subnet"
   }
